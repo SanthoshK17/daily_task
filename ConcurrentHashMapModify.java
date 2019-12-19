0dd7bf6b0dc478a1496a60e8dbce0e0faf8627d0
SanthoshK17 <47643839+SanthoshK17@users.noreply.github.com>
2019-12-19 19:42:44 +0530

    Add files via upload

---
+++ ConcurrentHashMapModify.java
@@ -0,0 +1,44 @@
+package com;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapModify extends Thread {
+
+	static ConcurrentHashMap<Integer,String> l =  
+            new ConcurrentHashMap<Integer,String>(); 
+
+public void run() 
+{ 
+
+// Child add new element in the object 
+l.put(103,"D"); 
+
+try
+{ 
+ Thread.sleep(2000); 
+} 
+catch(InterruptedException e) 
+{ 
+ System.out.println("Child Thread going to add element"); 
+} 
+}
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		 l.put(100,"A"); 
+	        l.put(101,"B"); 
+	        l.put(102,"C"); 
+	        ConcurrentHashMapModify t=new ConcurrentHashMapModify(); 
+	        t.start(); 
+	          
+	        for (Object o : l.entrySet())  
+	        { 
+	            Object s=o; 
+	            System.out.println(s); 
+	            Thread.sleep(1000); 
+	        } 
+	        System.out.println(l); 
+	}
+
+}
